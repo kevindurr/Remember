ff727b3baff8332819117218ea47680df71d6796
kevindurr <36747529+kevindurr@users.noreply.github.com>
2018-10-02 17:54:01 -0400

    added contextmenu functionality

--- Remember/background.js
+++ Remember/background.js
@@ -7,6 +7,30 @@ var background = {
                 background[request.fn](request,sender,sendResponse);
             }            
         });
+        var contextMenuItem = {
+            "id": "rememberThis",
+            "title": "Remember",
+            "contexts": ["selection"]
+        };
+        
+        chrome.contextMenus.create(contextMenuItem);
+        chrome.contextMenus.onClicked.addListener(function(clickData){
+            if (clickData.menuItemId == "rememberThis" && clickData.selectionText){
+                theurl = "http://127.0.0.1:5000/input?q="
+                theurl += clickData.selectionText
+                $.ajax({
+                    url: theurl,
+                    success: function(data){
+                        console.log(theurl);
+                        alert(data.txt);
+                    },
+                    error: function(e){
+                        alert(e.message);
+                    }
+                });
+                    // console.log(clickData.selectionText);
+            }
+        })
     },
 
     setCell: function(request,sender,sendResponse) {
